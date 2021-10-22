cf0531ccd2ee716f20103ecd732bcac16adbbc8a
Thomas Lohse <thomas.k.lohse@gmail.com>
2021-10-22 21:09:01 +0200

    init dev

---
+++ quantum/version.h
@@ -0,0 +1,9 @@
+/* This file was automatically generated. Do not edit or copy.
+ */
+
+#pragma once
+
+#define QMK_VERSION "0.12.29-12-g7a845a"
+#define QMK_BUILDDATE "2021-10-22-21:07:38"
+#define CHIBIOS_VERSION "2021-10-22-21:07:38"
+#define CHIBIOS_CONTRIB_VERSION "2021-10-22-21:07:38"
