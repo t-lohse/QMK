2a801d6f3b59e7044648f012a5156bfe79adc745
Thomas Lohse <thomas.k.lohse@gmail.com>
2021-12-31 15:52:39 +0100

    3key: upd, ascii-writer

--- keyboards/3key/keymaps/LohseBoi/keymap.c
+++ keyboards/3key/keymaps/LohseBoi/keymap.c
@@ -5,9 +5,17 @@
 #define GREEN 85
 #define BLUE 169
 
-#define ASCII_CODE
+#define ASCII_WRITER
+#ifdef ASCII_WRITER
 uint8_t asciiChar = 0;
 uint8_t asciiCounter = 0;
+
+uint16_t asciiTimer = 0;
+uint16_t asciiLimit = 200;
+
+uint8_t asciiClick = 0;
+bool asciiHolding = false;
+#endif
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	LAYOUT(KC_1, KC_2, KC_3)
@@ -26,6 +34,26 @@ void matrix_init_user(void) {
 }
 
 void matrix_scan_user(void) {
+#ifdef ASCII_WRITER
+    if (asciiClick > 0) {
+        if (timer_elapsed(asciiTimer) >= asciiLimit) {
+            if (asciiClick == 1) { //Single-tap to send, Double-click to empty buffer only
+                char str[2] = {' ', 0};
+                sprintf(str, "%c", asciiChar);
+                send_string(str);
+            }
+            asciiChar = 0;
+            asciiCounter = 0;
+            asciiClick = 0;
+            asciiTimer = timer_read();
+        }
+    } else if (asciiHolding) {
+        if (timer_elapsed(asciiTimer) >= asciiLimit/4) {
+            register_code(KC_BSPC);
+            asciiHolding = false;
+        }
+    }
+#endif
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -53,7 +81,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
     }
 
-#ifdef ASCII_CODE
+#ifdef ASCII_WRITER
     switch (record->event.key.col) {
         case 0:  // Left
             if (!record->event.pressed) {
@@ -63,18 +91,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             } 
             break;
         case 1:  // Center
-            if (!record->event.pressed) {
-                char str[2] = {' ', 0};
-                sprintf(str, "%c", asciiChar);
-                send_string(str);
-                asciiChar = 0;
-                asciiCounter = 0;
+            if (record->event.pressed) { //Press
+                asciiClick++;
+                asciiHolding = true;
+                asciiTimer = timer_read();
+            } else { //Release
+                unregister_code(KC_BSPC);
+                asciiHolding = false;
             } 
             break;
         case 2: // Right
             if (!record->event.pressed) {
                 asciiChar <<= 1;
-                asciiChar += 0;
                 asciiCounter++;
             } 
             break;
